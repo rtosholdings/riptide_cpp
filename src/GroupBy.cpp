rtosholdings/riptide_cpp

commit 00383adb98b3a21695bb8c14f152d0fa95a13292

fix ema decay and groupby mean with float32

src/GroupBy.cpp
@@ -518,8 +518,87 @@ public:
    }
 
 
+   // Just for float32 since we can upcast
+   //-------------------------------------------------------------------------------
+   static void AccumMeanFloat(void* pDataIn, void* pIndexT, INT32* pCountOut, void* pDataOut, INT64 len, INT64 binLow, INT64 binHigh, INT64 pass) {
+      T* pIn = (T*)pDataIn;
+      U* pOriginalOut = (U*)pDataOut;
+      V* pIndex = (V*)pIndexT;
+
+      // Allocate pOut
+      double* pOut = (double *)WORKSPACE_ALLOC(sizeof(double) * (binHigh - binLow));
+      if (pass <= 0) {
+         // Clear out memory for our range
+         memset(pOut, 0, sizeof(U) * (binHigh - binLow));
+      }
+
+      for (INT64 i = 0; i < len; i++) {
+         V index = pIndex[i];
+
+         //--------------------------------------
+         ACCUM_INNER_LOOP(index, binLow, binHigh)
+            double temp = pIn[i];
+            pOut[index - binLow] += (double)temp;
+            pCountOut[index]++;
+         }
+      }
+
+      if (pass < 0) {
+         for (INT64 i = binLow; i < binHigh; i++) {
+            if (pCountOut[i] > 0) {
+               pOriginalOut[i] = (U)(pOut[i-binLow] / (double)(pCountOut[i]));
+            }
+            else {
+               pOriginalOut[i] = GET_INVALID(pOriginalOut[i]);
+            }
+         }
+      }
+
+      WORKSPACE_FREE(pOut);
+   }
+
+
    //-------------------------------------------------------------------------------
    static void AccumNanMean(void* pDataIn, void* pIndexT, INT32* pCountOut, void* pDataOut, INT64 len, INT64 binLow, INT64 binHigh, INT64 pass) {
+      T* pIn = (T*)pDataIn;
+      U* pOriginalOut = (U*)pDataOut;
+      V* pIndex = (V*)pIndexT;
+
+      // Allocate pOut
+      double* pOut = (double*)WORKSPACE_ALLOC(sizeof(double) * (binHigh - binLow));
+      if (pass <= 0) {
+         // Clear out memory for our range
+         memset(pOut + binLow, 0, sizeof(U) * (binHigh - binLow));
+      }
+
+      for (INT64 i = 0; i < len; i++) {
+         V index = pIndex[i];
+
+         //--------------------------------------
+         ACCUM_INNER_LOOP(index, binLow, binHigh)
+            T temp = pIn[i];
+            if (temp == temp) {
+               pOut[index - binLow] += (U)temp;
+               pCountOut[index]++;
+            }
+         }
+      }
+
+      if (pass < 0) {
+         for (INT64 i = binLow; i < binHigh; i++) {
+            if (pCountOut[i] > 0) {
+               pOriginalOut[i] = (U)(pOut[i - binLow] / (double)(pCountOut[i]));
+            }
+            else {
+               pOriginalOut[i] = GET_INVALID(pOut[i]);
+            }
+         }
+      }
+      WORKSPACE_FREE(pOut);
+   }
+
+   //-------------------------------------------------------------------------------
+   static void AccumNanMeanFloat(void* pDataIn, void* pIndexT, INT32* pCountOut, void* pDataOut, INT64 len, INT64 binLow, INT64 binHigh, INT64 pass) {
       T* pIn = (T*)pDataIn;
       U* pOut = (U*)pDataOut;
       V* pIndex = (V*)pIndexT;
@@ -2131,7 +2210,7 @@ static GROUPBY_TWO_FUNC GetGroupByFunction(BOOL *hasCounts, INT32 *wantedOutputT
       *hasCounts = TRUE;
       switch (inputType) {
       case NPY_BOOL:   *wantedOutputType = NPY_DOUBLE; return GroupByBase < INT8,   double, V > ::AccumMean;
-      case NPY_FLOAT:  *wantedOutputType = NPY_FLOAT; return GroupByBase<float,   float, V>::AccumMean;
+      case NPY_FLOAT:  *wantedOutputType = NPY_FLOAT; return GroupByBase<float,   float, V>::AccumMeanFloat;
       case NPY_DOUBLE: *wantedOutputType = NPY_DOUBLE; return GroupByBase<double, double, V>::AccumMean;
       case NPY_LONGDOUBLE: *wantedOutputType = NPY_DOUBLE; return GroupByBase<long double, double, V>::AccumMean;
       case NPY_INT8:   *wantedOutputType = NPY_DOUBLE; return GroupByBase<INT8,     double, V>::AccumMean;
@@ -2150,7 +2229,7 @@ static GROUPBY_TWO_FUNC GetGroupByFunction(BOOL *hasCounts, INT32 *wantedOutputT
       *hasCounts = TRUE;
       switch (inputType) {
       case NPY_BOOL:   *wantedOutputType = NPY_DOUBLE; return GroupByBase < INT8, double, V > ::AccumNanMean;
-      case NPY_FLOAT:  *wantedOutputType = NPY_FLOAT; return GroupByBase<float, float, V>::AccumNanMean;
+      case NPY_FLOAT:  *wantedOutputType = NPY_FLOAT; return GroupByBase<float, float, V>::AccumNanMeanFloat;
       case NPY_DOUBLE: *wantedOutputType = NPY_DOUBLE; return GroupByBase<double, double, V>::AccumNanMean;
       case NPY_LONGDOUBLE: *wantedOutputType = NPY_DOUBLE; return GroupByBase<long double, double, V>::AccumNanMean;
       case NPY_INT8:   *wantedOutputType = NPY_DOUBLE; return GroupByBase<INT8, double, V>::AccumNanMean;

src/Ema.cpp
@@ -5,6 +5,15 @@
 #include "Ema.h"
 #include <stdio.h>
 #include <cmath>
+#include <pymem.h>
+//#include <numpy/ndarraytypes.h>
+//#include "numpy/arrayobject.h"
+//#include <numpy/npy_common.h>
+//#include "npy_config.h"
+#include <../Lib/site-packages/numpy/core/include/numpy/ndarraytypes.h>
+#include <../Lib/site-packages/numpy/core/include/numpy/arrayobject.h>
+#include <../Lib/site-packages/numpy/core/include/numpy/npy_common.h>
+//#include <../Lib/site-packages/numpy/npy_config.h>
 
 #define LOGGING(...)
 //#define LOGGING printf
@@ -973,6 +982,12 @@ public:
       // Default every LastTime bin to 0, including floats
       memset(pLastTime, 0, size);
 
+      size = (numUnique + GB_BASE_INDEX) * sizeof(T);
+      T* pLastValue = (T*)WORKSPACE_ALLOC(size);
+
+      // Default every LastValue bin to 0, including floats
+      memset(pLastValue, 0, size);
+
       U Invalid = GET_INVALID(pDest[0]);
 
       // Neiman's matlab loop below
@@ -1025,9 +1040,14 @@ public:
                   // NOTE: fill in last value
                   if (pIncludeMask[i] != 0) {
                      value = pSrc[i];
-                     pLastEma[location] = value + pLastEma[location] * exp(-decayRate * (pTime[i] - pLastTime[location]));
-                     pLastTime[location] = pTime[i];
                   }
+                  else {
+                     // Acts like fill forward
+                     value = pLastValue[location];
+                  }
+                  pLastEma[location] = value + pLastEma[location] * exp(-decayRate * (pTime[i] - pLastTime[location]));
+                  pLastTime[location] = pTime[i];
+                  pLastValue[location] = value;
                   pDest[i] = pLastEma[location];
                }
                else {
@@ -1079,6 +1099,7 @@ public:
 
       WORKSPACE_FREE(pLastTime);
       WORKSPACE_FREE(pLastEma);
+      WORKSPACE_FREE(pLastValue);
    }
 
 

commit fbfec5b551dabc7d2be685116b614bf3b171572c

Fix partially-uninitialized memory issue in float32 groupby mean/nanmean introduced in 00383ad.

src/GroupBy.cpp
@@ -529,7 +529,7 @@ public:
       double* pOut = (double *)WORKSPACE_ALLOC(sizeof(double) * (binHigh - binLow));
       if (pass <= 0) {
          // Clear out memory for our range
-         memset(pOut, 0, sizeof(U) * (binHigh - binLow));
+         memset(pOut, 0, sizeof(double) * (binHigh - binLow));
       }
 
       for (INT64 i = 0; i < len; i++) {
@@ -568,7 +568,7 @@ public:
       double* pOut = (double*)WORKSPACE_ALLOC(sizeof(double) * (binHigh - binLow));
       if (pass <= 0) {
          // Clear out memory for our range
-         memset(pOut + binLow, 0, sizeof(U) * (binHigh - binLow));
+         memset(pOut, 0, sizeof(double) * (binHigh - binLow));
       }
 
       for (INT64 i = 0; i < len; i++) {