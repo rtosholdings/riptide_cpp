05dd8ca95d29f8a8c9ae1b044afda85e8e31099d
staffantj <8728287+staffantj@users.noreply.github.com>
2021-09-27 14:51:43 -0400

    Operation traits for single parameter requests (#46)
    * Set up standards compliant compiles for C++17
    
    * Fix switch spelling on Windows
    
    * The numpy interface takes writeable strings, so we need to allow that non-standard behaviour in order to use literals
    
    * Still trying to activate '17
    
    * Pencil sketch with possible implementation avenues for discussion
    
    * RIP-537 More work along the road
    
    * Now to try to tie the return types back to the top
    
    * I think I've found the iteration sweet-spot. Now to get the loop(s) in place
    
    * Initial attempt at the slow loop
    
    * Initial attempt at the slow loop #2
    
    * It may be argued that this is scrolling through the output buffer with the wrong object size. Certainly a discussion worthy topic
    
    * Initial sketch of simd-coexistance in the processing loop. Tomorrow will see hopw that ends up looking when we put the matching functionality in the calculate functions. Also fixed up a couple of obscurely named simd functions (_mm256_set_epi64 -> _mm256_set_epi64x )
    
    * Wire in the simd shim where possible. This required some cleanup of the underlying traits classes as well.
    
    * gcc compile fix-up 1/n
    
    * gcc compile fix-up 2/n
    
    * gcc compile fix-up 3/n
    
    * gcc compile fix-up 4/n
    
    * Pick up whether we know the output type
    
    * Multi-dimensional / input-stride loops copied over from UnaryOps.cpp. It feels like there's something wrong with the existing code, but I'm not sure which is the correct paradigm, so I've left the behaviour alone
    
    * Just trying this out to see how the build farm reacts
    
    * Another experiment
    
    * Another experiment - this looks like it may be the state of the art in the farm
    
    * Reverting 7d90629 916b339 824eeb1 (reverting the experiment)
    
    * Attempt to fix up python line endings as well
    
    * Beware mixing expressions nad decltype(auto) return types
    
    Co-authored-by: Staffan Tjernstrom <staffantj@users.noreply.github.com>

---
+++ src/one_input.h
@@ -0,0 +1,26 @@
+#ifndef RIPTABLECPP_ONE_INPUT_H
+#define RIPTABLECPP_ONE_INPUT_H
+
+#include "RipTide.h"
+#include "MathWorker.h"
+#include "operation_traits.h"
+#include "ndarray.h"
+
+#include <variant>
+#include <optional>
+#include <utility>
+#include <cstddef>
+
+extern "C"
+{
+   PyObject * process_one_input( PyArrayObject const* in_array, PyArrayObject * out_object_1, int32_t function_num, int32_t numpy_intype, int32_t numpy_outtype = -1 );
+}
+
+
+namespace internal
+{
+   using chosen_traits_t = std::pair< std::optional< operation_t >, std::optional< data_type_t > >;
+
+   chosen_traits_t set_traits( int32_t const function_num, int32_t const numpy_intype );
+}
+#endif
---
+++ src/overloaded.h
@@ -0,0 +1,14 @@
+#ifndef RIPTABLECPP_OVERLOADED_H
+#define RIPTABLECPP_OVERLOADED_H
+
+namespace internal
+{
+   template< typename... Ts > struct overloaded : Ts...
+   {
+      using Ts::operator()...;
+   };
+
+   template< typename... Ts > overloaded( Ts... ) -> overloaded< Ts... >;
+}
+
+#endif
