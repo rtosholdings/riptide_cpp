2912194149e404c180c2bfd92d8ccaccfc427359
Orest Zborowski <orest.zborowski@sig.com>
2021-10-18 16:26:26 -0400

    Streamline Python test environment setup

--- cmake/AddPythonSupport.cmake
+++ cmake/AddPythonSupport.cmake
@@ -1,5 +1,18 @@
 find_package(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
 
+message(DEBUG "Python3_VERSION = ${Python3_VERSION}")
+message(DEBUG "Python3_VERSION_MAJOR = ${Python3_VERSION_MAJOR}")
+message(DEBUG "Python3_VERSION_MINOR = ${Python3_VERSION_MINOR}")
+message(DEBUG "Python3_EXECUTABLE = ${Python3_EXECUTABLE}")
+message(DEBUG "Python3_STDLIB = ${Python3_STDLIB}")
+message(DEBUG "Python3_SITELIB = ${Python3_SITELIB}")
+message(DEBUG "Python3_INCLUDE_DIRS = ${Python3_INCLUDE_DIRS}")
+message(DEBUG "Python3_LINK_OPTIONS = ${Python3_LINK_OPTIONS}")
+message(DEBUG "Python3_LIBRARIES = ${Python3_LIBRARIES}")
+message(DEBUG "Python3_LIBRARY_DIRS = ${Python3_LIBRARY_DIRS}")
+message(DEBUG "Python3_RUNTIME_LIBRARY_DIRS = ${Python3_RUNTIME_LIBRARY_DIRS}")
+
+
 ################################################################################
 # Determine the filename suffix and extension to use; the compiled extension (library)
 # will have this appended to the name so it's recognized as a CPython extension.
@@ -18,7 +31,5 @@ if(NOT _PYTHON_EXTENSION_SUFFIX_RESULT EQUAL "0")
   endif(WIN32)
 endif(NOT _PYTHON_EXTENSION_SUFFIX_RESULT EQUAL "0")
 
-get_filename_component(PYTHON_EXE_DIR ${Python3_EXECUTABLE} DIRECTORY )
-
 set(PYTHON_EXT_LIB_SUFFIX ${_PYTHON_EXTENSION_SUFFIX} CACHE STRING "The suffix of the C extension library")
-message(STATUS "Using Python C extension file suffix: ${PYTHON_EXT_LIB_SUFFIX}")
+message(DEBUG "Using Python C extension file suffix: ${PYTHON_EXT_LIB_SUFFIX}")
