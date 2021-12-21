24d07bcc857490e9c095794abb6ab4242114e316
Staffan Tjernstrom <staffantj@users.noreply.github.com>
2021-12-21 13:59:39 -0500

    Benchmark now runs on linux as well, due to aligning the simd call data (linux does vmovqda, not vmovqdu)

--- bench/riptide_bench/riptide_bench/bench_one_input.cpp
+++ bench/riptide_bench/riptide_bench/bench_one_input.cpp
@@ -11,24 +11,24 @@ namespace
     using namespace riptable_cpp::implementation;
     using namespace riptide::simd::avx2;
 
-    std::array<float const, 31> const input_data_simple_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   0.5, 1.0,
+  alignas(256) std::array<float const, 31> const input_data_simple_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   0.5, 1.0,
                                                               1.5, 2,    2.5, 3,    3.5, 4,    4.5,  5,    5.5, 6,   6.5,
                                                               7,   7.5,  8,   8.5,  9,   9.5,  10.5, 10.5, 11 };
 
-    std::array<float const, 31> const input_data_nan_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   NAN, 1.0,
+  alignas(256) std::array<float const, 31> const input_data_nan_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   NAN, 1.0,
                                                            1.5, 2,    2.5, 3,    3.5, 4,    4.5,  5,    5.5, 6,   6.5,
                                                            7,   7.5,  8,   8.5,  9,   9.5,  10.5, 10.5, 11 };
 
-    std::array<float const, 31> const input_data_inf_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   INFINITY, 1.0,
+  alignas(256) std::array<float const, 31> const input_data_inf_f = { -4,  -3.5, -3,  -2.5, -2,  -1.5, -1,   -0.5, 0,   INFINITY, 1.0,
                                                            1.5, 2,    2.5, 3,    3.5, 4,    4.5,  5,    5.5, 6,        6.5,
                                                            7,   7.5,  8,   8.5,  9,   9.5,  10.5, 10.5, 11 };
 
-    std::array<float const, 31> const input_data_normal_f = { -4,       -3.5,          -3,   -2.5, -2,  -1.5, -1,  -0.5, 0,
+  alignas(256) std::array<float const, 31> const input_data_normal_f = { -4,       -3.5,          -3,   -2.5, -2,  -1.5, -1,  -0.5, 0,
                                                               INFINITY, FLT_MIN / 2.0, NAN,  2,    2.5, 3,    3.5, 4,    4.5,
                                                               5,        5.5,           6,    6.5,  7,   7.5,  8,   8.5,  9,
                                                               9.5,      10.5,          10.5, 11 };
 
-    std::array<int32_t const, 31> const input_data_simple_i = { -8, -7, -6, -5, -4, -3, -2, -1, 0,  1,  2,  3,  4,  5,  6, 7,
+  alignas(256) std::array<int32_t const, 31> const input_data_simple_i = { -8, -7, -6, -5, -4, -3, -2, -1, 0,  1,  2,  3,  4,  5,  6, 7,
                                                                 8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 };
 
     float const * p_float = input_data_simple_f.data();
@@ -45,7 +45,7 @@ namespace
         int32_traits data_type{};
         for (auto _ : state)
         {
-            auto x = calculate(reinterpret_cast<char const *>(p_int32 + 5), &op, &data_type, vec256<int32_t>{});
+            auto x = calculate(reinterpret_cast<char const *>(p_int32), &op, &data_type, vec256<int32_t>{});
             benchmark::DoNotOptimize(x);
             benchmark::ClobberMemory();
         }
@@ -59,7 +59,7 @@ namespace
         float_traits data_type{};
         for (auto _ : state)
         {
-            auto x = calculate(reinterpret_cast<char const *>(p_float + 5), &op, &data_type, vec256<float>{});
+            auto x = calculate(reinterpret_cast<char const *>(p_float), &op, &data_type, vec256<float>{});
             benchmark::DoNotOptimize(x);
             benchmark::ClobberMemory();
         }
@@ -74,7 +74,7 @@ namespace
         std::array<float, 28> x{};
         for (auto _ : state)
         {
-            walk_data_array(1, 28, 4, 4, reinterpret_cast<char const *>(p_float + 5), reinterpret_cast<char *>(x.data()), op,
+            walk_data_array(1, 28, 4, 4, reinterpret_cast<char const *>(p_float), reinterpret_cast<char *>(x.data()), op,
                             data_type);
             benchmark::DoNotOptimize(x);
             benchmark::ClobberMemory();
@@ -90,7 +90,7 @@ namespace
         std::array<float, 28> x{};
         for (auto _ : state)
         {
-            walk_data_array(1, 28, 4, 4, reinterpret_cast<char const *>(p_float + 5), reinterpret_cast<char *>(x.data()), op,
+            walk_data_array(1, 28, 4, 4, reinterpret_cast<char const *>(p_float), reinterpret_cast<char *>(x.data()), op,
                             data_type);
             benchmark::DoNotOptimize(x);
             benchmark::ClobberMemory();
