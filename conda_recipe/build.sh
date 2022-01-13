9119d775fcff2c0b475ddb36507a12fdaa25db8a
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-01-13 13:10:46 -0500

    config is not spelt copnfig

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,4 +1,4 @@
 cmake -E make_directory "build"
 cmake -E chdir "build" cmake -DBENCHMARK_ENABLE_GTEST_TESTS=off -DCMAKE_BUILD_TYPE=Release -DRIPTIDE_PYTHON_VER="3.9.9" ../
-cmake --build "build" --copnfig Release
+cmake --build "build" --config Release
 
