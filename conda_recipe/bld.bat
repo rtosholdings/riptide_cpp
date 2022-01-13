f88b912cf6036663265fa0cf3ca3726c2df35ce6
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-01-13 13:26:47 -0500

    Windows is also on Python 3.9.9

--- conda_recipe/bld.bat
+++ conda_recipe/bld.bat
@@ -1,4 +1,4 @@
 cmake -E make_directory "build"
-cmake -E chdir "build" cmake -DBENCHMARK_ENABLE_GTEST_TESTS=off -DCMAKE_BUILD_TYPE=Release -DRIPTIDE_PYTHON_VER="3.9.7" ../
-cmake --build "build" --copnfig Release
+cmake -E chdir "build" cmake -DBENCHMARK_ENABLE_GTEST_TESTS=off -DCMAKE_BUILD_TYPE=Release -DRIPTIDE_PYTHON_VER="3.9.9" .1./
+cmake --build "build" --config Release
 
