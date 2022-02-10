facd5bda123b91dc08a0d47327e07e8c860d7f24
Orest Zborowski <orest.zborowski@sig.com>
2022-02-10 13:47:37 -0500

    Use VS16,VC14.29,v142

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-import sys
-
 from skbuild import setup
-
-from platform import python_version
+import platform
 
 package_name = 'riptide_cpp'
 
-riptide_python_ver_pieces = [ "-DRIPTIDE_PYTHON_VER=",python_version()]
+riptide_python_ver_pieces = [ "-DRIPTIDE_PYTHON_VER=", platform.python_version()]
 riptide_python_ver = "".join(riptide_python_ver_pieces)
+
+cmake_args = [
+    '-DBENCHMARK_ENABLE_GTEST_TESTS=off',
+     riptide_python_ver
+     ]
+
+if platform.system() == 'Windows':
+    cmake_args += [
+        '-GVisual Studio 16 2019',
+        '-Tv142,version=14.29,host=x64'
+        ]
 
 setup(
     name = package_name,
@@ -15,7 +23,7 @@ setup(
         'root': '.',
         'version_scheme': 'post-release',
     },
-    cmake_args = ['-DBENCHMARK_ENABLE_GTEST_TESTS=off',riptide_python_ver],
+    cmake_args = cmake_args,
     cmake_install_dir='src',
     setup_requires=['setuptools_scm'],
     description = 'Python Package with fast math util functions',
