937f377b4e4b94a70347bdc66cc186f6875bbc0d
OrestZborowski-SIG <orest.zborowski@sig.com>
2022-02-11 14:09:08 -0500

    Improve local builds (#94)
    Fix scikit-build to generate good riptide_cpp package.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-import sys
-
 from skbuild import setup
-
-from platform import python_version
+import platform
 
 package_name = 'riptide_cpp'
 
-riptide_python_ver_pieces = [ "-DRIPTIDE_PYTHON_VER=",python_version()]
-riptide_python_ver = "".join(riptide_python_ver_pieces)
+cmake_args = [
+     "-DRIPTIDE_PYTHON_VER=" + platform.python_version()
+     ]
+
+if platform.system() == 'Windows':
+    cmake_args += [
+        '-GVisual Studio 16 2019'
+        ]
+elif platform.system() == 'Linux':
+    cmake_args += [
+        '-DCMAKE_C_COMPILER=gcc',
+        '-DCMAKE_CXX_COMPILER=g++'
+        ]
 
 setup(
     name = package_name,
@@ -15,8 +23,7 @@ setup(
         'root': '.',
         'version_scheme': 'post-release',
     },
-    cmake_args = ['-DBENCHMARK_ENABLE_GTEST_TESTS=off',riptide_python_ver],
-    cmake_install_dir='src',
+    cmake_args = cmake_args,
     setup_requires=['setuptools_scm'],
     description = 'Python Package with fast math util functions',
     author = 'RTOS Holdings',
