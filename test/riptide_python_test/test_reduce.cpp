0fe4bc2cf9b95fa57aca3d4709a14e0e60a7e328
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-02-18 13:38:20 -0500

    on the way to test_reduce

--- test/riptide_python_test/test_reduce.cpp
+++ test/riptide_python_test/test_reduce.cpp
@@ -15,10 +15,12 @@ namespace
         "reduce_nanmean_"_test = [&]
         {
             std::array<char,6> test_values{'a','a','a','b','c'};
+            npy_intp one_dim{1};
+            PyObject * array{ PyArray_SimpleNewFromData( 5, &one_dim, NPY_BYTE, test_values.data() ) };
             PyObject * function_object = riptide_python_test::internal::get_named_function( riptide_module_p, "Reduce" );
-            PyObject * retval = PyObject_CallFunction(function_object, "d", 3.142);
 
-            expect(retval != nullptr);
+            expect(array != nullptr);
+            expect(function_object != nullptr);
         };
     };
 }
