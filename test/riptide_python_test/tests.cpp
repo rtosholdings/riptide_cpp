0de06e303823763db8431c174de53853338ffc34
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-02-18 14:06:08 -0500

    Updated file names

--- test/riptide_python_test/tests.cpp
+++ test/riptide_python_test/tests.cpp
@@ -10,6 +10,18 @@ using boost::ut::suite;
 
 namespace
 {
+    suite riptide_ops = []
+    {
+        "test_numpy_float"_test = [&]
+        {
+            PyObject * function_object = riptide_python_test::internal::get_named_function( riptide_module_p, "TestNumpy" );
+            PyObject * retval = PyObject_CallFunction(function_object, "d", 3.142);
+
+            expect(function_object != nullptr);
+            expect(retval != nullptr);
+        };
+    };
+
     suite reduce_ops = []
     {
         "reduce_nanmean_"_test = [&]
