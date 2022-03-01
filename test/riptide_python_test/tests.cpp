0b23dbc39dc80185539e0d48d78ef6d703acc8fd
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-03-01 10:24:15 -0500

    body of nanmean test

--- test/riptide_python_test/tests.cpp
+++ test/riptide_python_test/tests.cpp
@@ -26,13 +26,19 @@ namespace
     {
         "reduce_nanmean_"_test = [&]
         {
-            std::array<char,6> test_values{'a','a','a','b','c'};
-            npy_intp one_dim{1};
-            PyObject * array{ PyArray_SimpleNewFromData( 5, &one_dim, NPY_BYTE, test_values.data() ) };
+            std::array<char,5> test_values{'a','a','a','b','c'};
+            npy_intp const dim_len{5};
+            PyObject * array{ PyArray_SimpleNew(1, &dim_len, NPY_BYTE ) };
             PyObject * function_object = riptide_python_test::internal::get_named_function( riptide_module_p, "Reduce" );
+            PyObject * reduce_fn_num{ Py_BuildValue( "i", 103 ) };
+            PyObject * arg_tuple{ PyTuple_Pack( 2, array, reduce_fn_num ) };
+            PyObject * retval = PyObject_CallObject(function_object, arg_tuple);
 
             expect(array != nullptr);
             expect(function_object != nullptr);
+            expect(reduce_fn_num != nullptr);
+            expect(arg_tuple != nullptr);
+            expect(retval != nullptr);
         };
     };
 }
