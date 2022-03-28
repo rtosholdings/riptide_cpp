69a2149d2d0b6eaad667b256179aa5a1656a9a4f
Staffan Tjernstrom <8728287+staffantj@users.noreply.github.com>
2022-03-28 13:19:20 -0400

    Declaration of template type for unit tests only needed on MSVC (due to dll symbols)?

--- src/HashLinear.cpp
+++ src/HashLinear.cpp
@@ -6175,4 +6175,6 @@ PyObject * MergeBinnedAndSorted(PyObject * self, PyObject * args)
     return (PyObject *)indexArray;
 }
 
+#ifdef RT_COMPILER_MSVC
 template CHashLinear<uint64_t, int64_t>;
+#endif
